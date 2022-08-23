4e4cc84d3d9f639e4705a531807cb105ee984a7a
GunchaPy <guntars.reiss@gmail.com>
2022-08-23 20:55:15 +0300

    Initial Commit

---
+++ Friendsbook/FriendsbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsbookApp.swift
+//  Friendsbook
+//
+//  Created by Guntars Reiss on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
